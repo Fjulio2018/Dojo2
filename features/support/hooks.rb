2b7bc957d0c845118e306f18b0e8039a49e4de1b
“Fjulio2018” <juliomariano2005@yahoo.com.br>
2018-04-10 15:08:05 -0300

    atualização do Hook

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,5 +1,8 @@
-After do |scenario|
-    page.save_screenshot '.testebefore.png'
 
- end 
+After do |scenario|
+ data = Time.now.strftime("%Y%m%dT%H%M%S")
+ page.save_screenshot 'Evidencia tasks '+data+'.png'
+end
+
+
 
